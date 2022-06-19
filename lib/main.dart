517293fb1dc2a4944d94c90c2ca56ccbb7834005
unknown <eam499@cs.soran.edu.iq>
2022-06-19 14:25:18 +0300

    KurdistanFLag

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,6 @@
 import 'package:flutter/material.dart';
+import 'src/kurdistan.dart';
 
 void main() {
-  runApp(hello());
-}
-
-class hello extends StatelessWidget {
-  const hello({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text("Hello Kuristan"),
-        ),
-      ),
-    );
-  }
+  runApp(helloKurdistan());
 }
